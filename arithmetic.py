36ae9ab9709aa5df26d1584fd2fedb97a7bda024
ChaMakTX <tchamak007@gmail.com>
2024-10-04 17:48:12 +0300

    created some math operations

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+#addition
+print(1 + 4)
+
+#Substraction
+print(5 - 10)
+
+#Multiplication
+print(9*10)
+
+#Division
+print(4/3)
+
+#Exponent
+print(5**2)
+
+#runnin
+print(((1 + 3) * (9 - 2)/2)**2)

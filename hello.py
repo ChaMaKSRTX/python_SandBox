30b953f622aa88e90c48ae287419a2c888c1d1cb
ChaMakTX <tchamak007@gmail.com>
2024-10-04 17:45:05 +0300

    first Hello in python and some python syntax

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Basic syntax and indentation
+print("hello world")
+
+""" is good to write code with a proper indentation as wrong indentation can result into an error"""
+if 5 > 2:
+    print("its true")#this is the right way to do it
+
+"""
+if 5 > 2:
+print("its true") #This is the wrong way to do it
+"""

1ce360617be54d3e4c4579453d3bf7b226694e49
ChaMakTX <tchamak007@gmail.com>
2024-10-19 12:23:32 +0300

    Operators and its manipulations

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+"""
+Variables can store data of different types, and different types can do different things.
+
+Python has the following data types built-in by default, in these categories:
+
+Text/ string Data Type:	str
+Numeric Data Types:	int, 
+                    float, 
+                    complex.
+Sequence Data Types:	list, 
+                        tuple, 
+                        range.
+Mapping Data Type:	dict
+Set Data Types:	set, 
+                frozenset.
+Boolean Data Type:	bool
+Binary Data Types:	bytes,
+                     bytearray, 
+                     memoryview.
+None Data Type:	NoneType
+
+"""
+
+#Text/ string Data Type:	str
+name = "Chamak"
+second_name = 'T*X'
+word = """ How can you fly to the moon"""
+l_word = '''Space ship'''
+#print(f"{name} {second_name},{word} i'll use {l_word}")
+
+#Numeric Data Types:
+a = 2 #int
+b = 4.6 #float
+c = 4j  #complex
+
+#print(a)
+#print(b)
+#print(c)
+#print(f"the type of variable having, {a} is, {type(a)}")
+#print(f"the type of variable having, {b} is, {type(b)}")
+#print(f"the type of variable having, {c} is, {type(c)}")
+
+#Sequence Data Types:
